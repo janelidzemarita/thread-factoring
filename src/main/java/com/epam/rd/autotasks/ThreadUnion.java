58bd3f509e1bb9afc5b96a98fe139d2eef02e131
M.J <janelidze.marita@kiu.edu.ge>
2023-08-15 13:44:40 +0300

    Initial Commit - Solution

--- src/main/java/com/epam/rd/autotasks/ThreadUnion.java
+++ src/main/java/com/epam/rd/autotasks/ThreadUnion.java
@@ -15,6 +15,8 @@ public interface ThreadUnion extends ThreadFactory {
     List<FinishedThreadResult> results();
 
     static ThreadUnion newInstance(String name){
-        throw new UnsupportedOperationException();
+
+//        throw new UnsupportedOperationException();
+        return new ThreadUnionImpl(name);
     }
 }
